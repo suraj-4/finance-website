0985c53c9808c377e17014878ad74ea5591284ef
suraj-4 <105719685+suraj-4@users.noreply.github.com>
2023-08-26 21:39:57 +0530

    All js file uploaded

---
+++ custom.js
@@ -0,0 +1,119 @@
+//Banner section
+$('.hero').owlCarousel({
+    items:1,
+    loop: true,
+    margin:0,
+    nav:false,
+    dots:false,
+    autoplayHoverPause:true,
+    autoHeight:false,
+    autoplay :true,
+    autoplayTimeout :2000
+});
+
+// counter section
+$('.count').counterUp({
+    delay:20,
+    time:2000
+});
+
+// expert section
+$('.owl-two').owlCarousel({
+    items: 2,
+    margin: 30,
+    loop: true,
+    nav : true,
+    dots: false,
+    center : false,
+    autoplayHoverPause:true,
+    autoplay :true,
+    autoplayTimeout :2000,
+    // stagePadding : 80, // in px
+    responsive:{
+        0:{
+            items:1,
+            responsiveClass : true
+        },
+        600:{
+            items:1,
+        },
+        1000:{
+            items:2,
+        }
+    }
+});
+// testimonial section
+$('.owl-three').owlCarousel({
+    items: 2,
+    margin: 30,
+    loop: true,
+    nav : false,
+    dots: true,
+    center : false,
+    autoplayHoverPause:true,
+    autoplay :true,
+    autoplayTimeout :2000,
+    // stagePadding : 80, // in px
+    responsive:{
+        0:{
+            items:1,
+            responsiveClass : true
+        },
+        600:{
+            items:1,
+        },
+        1000:{
+            items:2,
+        }
+    }
+});
+// Latest News section
+$('.owl-forth').owlCarousel({
+    items: 3,
+    margin: 30,
+    loop: true,
+    autoplayHoverPause:true,
+    nav : false,
+    dots: true,
+    center : false,
+    autoplay :true,
+    autoplayTimeout :2000,
+    // stagePadding : 80, // in px
+    responsive:{
+        0:{
+            items:1,
+            responsiveClass : true
+        },
+        600:{
+            items:1,
+        },
+        1000:{
+            items:3,
+        }
+    }
+});
+// our partner section
+$('.owl-fifth').owlCarousel({
+    items: 3,
+    margin: 30,
+    loop: true,
+    autoplayHoverPause:true,
+    nav : false,
+    dots: false,
+    center : false,
+    autoplay :true,
+    autoplayTimeout :2000,
+    // stagePadding : 80, // in px
+    responsive:{
+        0:{
+            items:1,
+            responsiveClass : true
+        },
+        600:{
+            items:1,
+        },
+        1000:{
+            items:3,
+        }
+    }
+});
